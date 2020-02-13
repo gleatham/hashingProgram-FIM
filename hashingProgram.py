ded305dbd5d8ce5b10baa65d9698ea727a7ce0a9
gleatham <leatham.geoffrey@gmail.com>
2020-02-13 09:40:53 -0800

    createHash() accepts and returns a dictionary

--- hashingProgram.py
+++ hashingProgram.py
@@ -3,41 +3,52 @@ import hashlib
 
 
 # Creat hash of a directory
-def createHash(directory):
-    SHAhash = hashlib.md5()
+def createHash(directory, numDirectories):
+    newHash = {}
     directory = directory
+    numDirectories = numDirectories
     #directory = 'C:\\Users\\Geoff\\Documents\\hashTest'
+    keys = []
+    keys = list(directory.keys())
+    print(keys[0])
 
+    count = 0
+    while(count < numDirectories):
+        tempDir = directory.get(keys[count])
+        SHAhash = hashlib.md5()
+        print("Hashing: ", tempDir)
+        try:
+            for root, dirs, files in os.walk(tempDir):
+                for names in files:
+                    filepath = os.path.join(root, names)
+                    try:
+                        fin = open(filepath, 'rb')
+                    except:
+                        print("Unable to open file")
+                        fin.close()
+                        continue
 
-    try:
-        for root, dirs, files in os.walk(directory):
-            for names in files:
-                filepath = os.path.join(root, names)
-                try:
-                    fin = open(filepath, 'rb')
-                except:
-                    print("Unable to open file")
+                    var = True
+                    while var:
+                        # Read file in chunks
+                        buf = fin.read(4096)
+                        if not buf:
+                            break
+                        #SHAhash.update(hashlib.md5(buf).hexdigest())
+                        SHAhash.update(hashlib.md5(buf).digest())
+
                     fin.close()
-                    continue
 
-                var = True
-                while var:
-                    # Read file in chunks
-                    buf = fin.read(4096)
-                    if not buf:
-                        break
-                    #SHAhash.update(hashlib.md5(buf).hexdigest())
-                    SHAhash.update(hashlib.md5(buf).digest())
-                fin.close()
+        except:
+            print("Hashing failed")
+            import traceback
+            traceback.print_exc(limit=None, file=None, chain=True)
+            return -2
+        newHash.update({keys[count]: SHAhash})
+        count+=1
 
-
-    except:
-        print("Hashing failed")
-        import traceback
-        traceback.print_exc(limit=None, file=None, chain=True)
-        return -2
-
-    return SHAhash.hexdigest()
+    print("leaving createHash()....", newHash.keys())
+    return newHash
 
 #Reads in a directory path from a file.
 #Directory path is used in createHash()
@@ -45,18 +56,23 @@ def getDirectory():
     print("Getting the directory from file")
     directories = {}
 
-    with open('C:\\Users\\Geoff\\Documents\\hashTest\\directories.txt') as fin:
-        directoriesList = fin.readlines()
-        numDirectories = len(directoriesList)
+    try:
+        with open('C:\\Users\\Geoff\\Documents\\hashTest\\directories.txt') as fin:
+            directoriesList = fin.readlines()
+            numDirectories = len(directoriesList)
 
-        counter = 0
-        while(counter < numDirectories):
-            tempDirList = directoriesList[counter].split(':', 3)
+            counter = 0
+            while(counter < numDirectories):
+                tempDirList = directoriesList[counter].split(':', 3)
 
-            directories.update({tempDirList[0]: tempDirList[1]})
+                directories.update({tempDirList[0]: tempDirList[1]})
 
-            counter+=1
-
+                counter+=1
+    except:
+        print("getDirectory() failed")
+        import traceback
+        traceback.print_exc(limit=None, file=None, chain=True)
+        return -2
 
     return directories
 
@@ -64,20 +80,23 @@ def getDirectory():
 def getHashes():
     print("Get Current Hashes")
     hashes = {}
+    try:
+        with open('C:\\Users\\Geoff\\Documents\\hashTest\\directories.txt') as fin:
+            hashesList = fin.readlines()
+            numDirectories = len(hashesList)
 
-    with open('C:\\Users\\Geoff\\Documents\\hashTest\\directories.txt') as fin:
-        hashesList = fin.readlines()
-        numDirectories = len(hashesList)
+            counter = 0
+            while(counter < numDirectories):
+                tempHashList = hashesList[counter].split(':', 3)
 
-        counter = 0
-        while(counter < numDirectories):
-            tempHashList = hashesList[counter].split(':', 3)
+                hashes.update({tempHashList[0]: tempHashList[2]})
 
-            hashes.update({tempHashList[0]: tempHashList[2]})
-
-            counter+=1
-
-
+                counter+=1
+    except:
+        print("getHashes() Failed")
+        import traceback
+        traceback.print_exc(limit=None, file=None, chain=True)
+        return -2
     return hashes
 
 def compareHash():
@@ -85,23 +104,17 @@ def compareHash():
 
 
 def main():
-    print("In Main")
-
-    #add C: to directories for windows
-
     #Dictionaries for directory path and hash
     directories = getDirectory()
     hashes = getHashes()
 
-    #Below commented code works. Refactoring to accept dictionary.
-    newHash = createHash(directories.get('folder1'))
-#
-    #newHash = createHash(directories)
+    numDirectories = len(directories)
+    newHash = createHash(directories, numDirectories)
 
-    print(newHash)
-    #newHash = createHash(directories)
-    #test = createHash()
-    #print(test)
+    #prints the hash to the screen
+    print(newHash.get('folder1').hexdigest())
+    print(newHash.get('folder2').hexdigest())
+    print(newHash.get('folder3').hexdigest())
 
 if __name__ == "__main__":
     main()
---
+++ test.py
@@ -0,0 +1,10 @@
+def iterateArray(arrOne):
+    arrOne = arrOne
+    print("Iterate")
+
+def main():
+    print("Main")
+
+
+if __name__ == "__main__":
+    main()
